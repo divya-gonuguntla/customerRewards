1959ae5314a78b0fd4145b7a25b780fbe1036051
divya-gonuguntla <123658460+divya-gonuguntla@users.noreply.github.com>
2023-01-29 13:00:58 +0530

    Delete Constants.js

--- src/Constants.js
+++
@@ -1,6 +0,0 @@
-export const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-export const HEADER_ROW_NAMES = ["Customer Name","Num Of Transactions","Month","Rewards"]
-export const HISTORY_HEADER_ROW_NAMES = ["Transaction Date","Amount","Rewards"]
-export const CUSTOMER_AGGREGATE_TABLE_HEADERS=["Name","Rewards"]
-export const CUSTOMER_REWARDS_BY_MONTH_TITLE='Customer Rewards by Month'
-export const CUSTOMER_REWARDS_TITLE='Customer Rewards'
